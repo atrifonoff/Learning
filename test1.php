a52d1f1e3aeabc2c9bc2f75173b90ad3989a76cd
angel <angel.trifonoff@gmail.com>
2017-06-20 14:27:04 +0300

    тест 1 качване

--- test1.php
+++ test1.php
@@ -1,3 +1,34 @@
 <?php
-echo "ala bala";
+$line = trim(fgets(STDIN));
+$nums = explode(" ",$line);
+$nums = array_map("intval",$nums);
+$data = [];
+$sequences = [];
+$counter=1;
+for($i = 0; $i < (count($nums)); ++$i)
+{
+    if(count($nums)==1)
+    {
+        $sequences[]=$data=array('element'=>$nums[$i],'cout'=>$counter);break;}
+        if($nums[$i] == $nums[$i+1])
+        {
+            $counter++;
+            if($i == (count($nums)-2))
+            {
+                $sequences[]=$data=array('element'=>$nums[$i],'cout'=>$counter);break;}
+        } else{$sequences[]=$data=array('element'=>$nums[$i],'cout'=>$counter);$counter = 1;
+        if($i == (count($nums)-2))
+        {
+            $sequences[]=$data=array('element'=>$nums[$i],'cout'=>$counter);break;};
+    }
+}
+$maxSequens = $sequences[0];
+foreach ($sequences as $key){
+    //echo $key['element']." => ".$key['cout']."\n";
+                if($key['cout'] > $maxSequens['cout']){$maxSequens = $key;}
+}
+//print_r($sequences);
+//print_r($maxSequens);
+echo "\n".str_repeat($maxSequens['element']." ",$maxSequens['cout']) ;
+
 ?>
