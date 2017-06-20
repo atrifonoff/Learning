1130f72a0ca94c38e3e7da0cbf47915102c6f073
angel <angel.trifonoff@gmail.com>
2017-06-20 13:49:11 +0300

    Commit class Battle за играта

--- Battle.php
+++ Battle.php
@@ -8,5 +8,52 @@
  */
 class Battle
 {
+    const ROUNDS = 10;
+
+    private $playerOne;
+    private $playerTwo;
+    private $winner;
+    private $rounds;
+
+
+    public function __construct(Player $playerOne,
+                                Player $playerTwo,
+                                $rounds = self::ROUNDS)
+    {
+        $this ->playerOne = $playerOne;
+        $this ->playerTwo = $playerTwo;
+        $this ->rounds = $rounds;
+
+
+    }
+
+    public function start()
+        {
+            $rounds = $this ->rounds;
+            $player1 = $this ->playerOne;
+            $player2 = $this ->playerTwo;
+            while ($rounds > 0  && $player1 ->isAlive() && $player2 ->isAlive())
+                {
+                    $player1->attack($player2);
+                    $player2->attack($player1);
+
+                    $rounds--;
+                }
+        }
+
+
+    public function getRsult()
+    {
+        if($this ->playerOne ->isAlive() == $this ->playerTwo ->isAlive())
+        {
+           return null;exit;
+        }
+        $winner = $this ->playerOne ->isAlive()
+            ? $this ->playerOne
+            : $this ->playerTwo;
+
+        return $winner;
+
+    }
 
 }
