e874150831677f841a90290259601ebfea573da9
atrifonoff <angeel.trifonoff@gmail.com>
2017-06-20 17:38:41 +0300

    Some change

--- test.php
+++ test.php
@@ -1,4 +1,53 @@
+
+<form method="get" onsubmit="">
+
+    <input type="text" name="data" />
+    <br>
+    <input type="text" name="coll" />
+    <input type="submit" value="colls"/>
+</form>
+
 <?php
-echo " test1";
-echo "This was wroten on git";
+if(isset($_GET['coll'])):
+    $data=explode(' ',$_GET['data']);
+     $elementCount=count($data);
+    $i=0;
+    $colls=($_GET['coll']-1);
+    $rows=ceil($elementCount/($colls+1));
+
+    for($r=0;$r<=$rows-1;$r++){
+        for($c=0;$c<=$colls;$c++){
+
+            $array[$r][$c]=$data[$i];
+            $i++;
+            if($i>=$elementCount){$i=0;echo '<br>'; break;}
+        }
+    }
+    echo '<pre>';
+    print_r($array);
+    //var_dump($array);
+    echo '</pre>';
+
+    echo 'elementCount= '.$elementCount.'<br>';
+        echo 'rows= '.$rows.'<br>';
+
+    for($r=0;$r<=$rows-1;$r++){
+        for($c=0;$c<=$colls;$c++){
+
+            echo $array[$r][$c].' ';
+
+            if($c==($colls)){echo '<br>';}
+            $i++;
+            if($i>=$elementCount){$i=0;echo '<br>'; break;}
+        }
+    }
+
+    foreach ($array as $row){
+        foreach ($row as $coll){
+            if(strlen($coll)>5):echo $coll.'<br>';endif;
+        }
+    }
+
+endif;
+
 ?>
