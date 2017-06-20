a2cfbc76588e24ff0ebc9952a51d6d0723637589
atrifonoff <angeel.trifonoff@gmail.com>
2017-06-20 18:15:49 +0300

    Creat folder "BattleInSchool" and remove files in

---
+++ BattleInSchool/Battle.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: angel
+ * Date: 19.06.17
+ * Time: 20:12
+ */
+class Battle
+{
+    const ROUNDS = 10;
+
+    private $playerOne;
+    private $playerTwo;
+    private $winner;
+    private $rounds;
+
+
+    public function __construct(Player $playerOne,
+                                Player $playerTwo,
+                                $rounds = self::ROUNDS)
+    {
+        $this ->playerOne = $playerOne;
+        $this ->playerTwo = $playerTwo;
+        $this ->rounds = $rounds;
+        $this ->winner;
+
+    }
+
+    public function start()
+        {
+            $rounds = $this ->rounds;
+            $player1 = $this ->playerOne;
+            $player2 = $this ->playerTwo;
+            while ($rounds > 0  && $player1 ->isAlive() && $player2 ->isAlive())
+                {
+                    $player1->attack($player2);
+                    $player2->attack($player1);
+
+                    $rounds--;
+                }
+        }
+
+
+    public function getResult()
+    {
+        if($this ->playerOne ->isAlive() == $this ->playerTwo ->isAlive())
+        {
+           return null;
+        }
+        $winner = $this ->playerOne ->isAlive()
+            ? $this ->playerOne
+            : $this ->playerTwo;
+
+        return $winner;
+
+    }
+
+}
---
+++ BattleInSchool/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'Player.php';
+require_once 'Battle.php';
+
+?>
+
+<form>
+
+   Player 1 name:  <input type="text" name="player_one_name" /><br>
+    Player 2 name:  <input type="text" name="player_two_name" /><br>
+    <input type="submit" name="start" value="Start Battle">
+
+
+</form>
+
+
+    <?php
+if(isset($_GET['start']))
+{
+    $player1 = new Player($_GET['player_one_name']);
+    $player2 = new Player($_GET['player_two_name']);
+    $battle = new Battle($player1,$player2);
+    $battle ->start();
+
+    if($battle ->getResult() === null)
+    {
+        echo 'Drow battle !';
+    } else{ echo 'The winner is: '.$battle ->getResult() ->getName();}
+}
+
