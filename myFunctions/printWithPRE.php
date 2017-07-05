f129b87472502c47db52d383a8fc62209dc46057
angel <angel.trifonoff@gmail.com>
2017-07-05 18:06:00 +0300

    Declare fuction printWithPRE

---
+++ myFunctions/printWithPRE.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: angel
+ * Date: 05.07.17
+ * Time: 18:03
+ */
+
+/* Декларирам си функции за по -добра визия при отпечатване на масиви  */
+
+function printrWithPre(array $arr){
+    echo '<pre>';
+    print_r($arr);
+    echo '</pre>';
+}
+function vardumpWithPre(array $arr){
+    echo '<pre>';
+    var_dump($arr);
+    echo '</pre>';
+}
