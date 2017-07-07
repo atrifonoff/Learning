3b8fac5e50a05747f5262d3086d0873e963ae0ff
atrifonoff <angeel.trifonoff@gmail.com>
2017-07-07 17:56:27 +0300

    New changelist

---
+++ test.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krisko
+ * Date: 06.07.17
+ * Time: 16:57
+ */
+//function vardumpWithPre(array $arr){
+//    echo '<pre>';
+//    var_dump($arr);
+//    echo '</pre>';
+//}
+//?>
+<!--<div>-->
+<!--    <form method="get" action="test.php">-->
+<!--        <input type="text" name="numberOfCars"></br>-->
+<!--        <input type="text" name='car' >-->
+<!--        <input type="submit" name="submit"></br>-->
+<!--        <input type="submit" name="stop" value="stop"></br>-->
+<!--    </form>-->
+<!--</div>-->
+<!---->
+<?php
+//$cars =[];
+//if (isset($_GET['submit'])) {
+//    echo $_GET['car'];
+//    $cars[]=$_GET['car'];
+//    vardumpWithPre($cars);
+//    if (isset($_GET['stop'])){
+//
+//    }
+//    unset($_GET['car']);
+//}
+//?>
+<!---->
