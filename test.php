70f9e8a84663bb263d34924847821bb8c3b01187
atrifonoff <angeel.trifonoff@gmail.com>
2017-08-03 14:35:18 +0300

    commit myself

--- test.php
+++ test.php
@@ -1,45 +1,5 @@
 <?php
+$myfile = fopen("datework.php", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("datework.php"));
 
-function vardumpWithPre(array $arr){
-    echo '<pre>';
-    var_dump($arr);
-    echo '</pre>';
-}
-/**
- * Created by PhpStorm.
- * User: krisko
- * Date: 06.07.17
- * Time: 16:57
- */
-//function vardumpWithPre(array $arr){
-//    echo '<pre>';
-//    var_dump($arr);
-//    echo '</pre>';
-//}
-//?>
-<!--<div>-->
-<!--    <form method="get" action="test.php">-->
-<!--        <input type="text" name="numberOfCars"></br>-->
-<!--        <input type="text" name='car' >-->
-<!--        <input type="submit" name="submit"></br>-->
-<!--        <input type="submit" name="stop" value="stop"></br>-->
-<!--    </form>-->
-<!--</div>-->
-<!---->
-<?php
-
-vardumpWithPre($__SERVER);
-
-
-//$cars =[];
-//if (isset($_GET['submit'])) {
-//    echo $_GET['car'];
-//    $cars[]=$_GET['car'];
-//    vardumpWithPre($cars);
-//    if (isset($_GET['stop'])){
-//
-//    }
-//    unset($_GET['car']);
-//}
-//?>
-<!---->
+fclose($myfile);
