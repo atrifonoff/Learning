056f121085aacf140a38bc085bccf529c7daa69b
angel <angel.trifonoff@gmail.com>
2017-08-09 23:03:15 +0300

    test With empyLineCounter

--- test.php
+++ test.php
@@ -1,5 +1,27 @@
 <?php
-$myfile = fopen("datework.php", "r") or die("Unable to open file!");
-echo fread($myfile,filesize("datework.php"));
+//$myfile = fopen("datework.php", "r") or die("Unable to open file!");
+//echo fread($myfile,filesize("datework.php"));
+//
+//fclose($myfile);
+$emptyLineCounter = 0;
+$handle = fopen("datework.php", "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
 
-fclose($myfile);
+        if((trim($line == NULL))){
+            echo 'empty'.$line."<br>";
+            $emptyLineCounter ++;
+
+        }
+
+    }
+    fclose($handle);
+
+} else {
+    echo 'error opening the file.';}
+
+echo 'Empty lines = '.$emptyLineCounter;
+
+
+
+
