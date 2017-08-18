d6c8016fff9e9bab505adb91622259e6e1973e9c
atrifonoff <angeel.trifonoff@gmail.com>
2017-08-18 12:45:41 +0300

    commit myself

--- myself/Codebase.class.php
+++ myself/Codebase.class.php
@@ -1,5 +1,7 @@
 <?php
+
 class myself_Codebase extends core_Manager
+
 {
     public $title = "Анализ";
     public $loadList = 'plg_Created,plg_Modified';
@@ -17,18 +19,21 @@ class myself_Codebase extends core_Manager
      * @param core_Manager $mvc
      * @param stdClass $data
      */
+
+
     public static function on_AfterPrepareListToolbar($mvc, &$res, $data)
     {
         $data->toolbar->addBtn('Анализ', array($mvc, 'ReadFiles'));
     }
 
-    function act_FileProcessing()
-    {
 
-        echo 'File processing';
-        bp();
-    }
 
+//    function act_FileProcessing()
+//    {
+//
+//        return 'File processing';
+//
+//    }
 
 
 
@@ -37,22 +42,35 @@ class myself_Codebase extends core_Manager
      */
     function act_ReadFiles()
     {
-        //Установява необходима роля за да се стартира екшъна //
+
+        /**
+         * Установява необходима роля за да се стартира екшъна
+         */
         requireRole('admin');
+
         //В $root запомняме директорията от която трябва да стартира екшъна.
         //В случая __DIR__ намира директорията на текущия файл и след това
         // DIRECTORY_SEPARATOR.'..' връща една dir назад//
+
+        /**
+         * Директория за стартиране
+         */
         $root = realpath(__DIR__ . DIRECTORY_SEPARATOR. '..' );
+
+
         $files = array();
         $methods = array();
         $usesMetods = array();
+        $onMethods = array();
+        $couldntLoadsClasses = array();
+        $onMethods_string = 0;
         $totalLines = 0;
         $totalLoadClasses = 0;
         $filesCounter = 0;
         $emptyLineCounter = 0;
         $totalMethods = 0;
 
-        $this->act_FileProcessing();
+//$this->act_FileProcessing();
 
 
 
@@ -62,7 +80,9 @@ class myself_Codebase extends core_Manager
 
             $filesCounter++;
 
-            if(self::loadClasses($root, $f, $methods, $couldntLoadsClasses)) {
+            if(self::loadClasses($root, $f, $methods,$couldntLoadsClasses, $onMethods)) {
+
+                $onMethods_string = implode($onMethods,"<br>");
 
                 $totalLoadClasses++;
 
@@ -96,27 +116,37 @@ class myself_Codebase extends core_Manager
 
             self::save($rec);
 
-            if($filesCounter >= 190) bp('Брой заредени класове :'.$totalLoadClasses,'Общ брой файлове :'
-                .$filesCounter,'Брой редове :'.$totalLines,'Брой празни редове :'.$emptyLineCounter,
-                'Брой методи :'.$totalMethods,'Не успях да заредя следните PHP-класове :'.$couldntLoadsClasses);
+//            if($filesCounter >= 190) bp('Общ брой файлове :'.$filesCounter,'Брой редове :'.$totalLines,'Брой празни редове :'.$emptyLineCounter,
+//                'Брой заредени класове :'.$totalLoadClasses,'Не успях да заредя следните PHP-класове :'.$couldntLoadsClasses,
+//                'Брой методи :'.$totalMethods,'on_Methods :'.count($onMethods), array_values($onMethods));
 
         }
+//        bp('Общ брой файлове :'.$filesCounter,'Брой редове :'.$totalLines,'Брой празни редове :'.$emptyLineCounter,
+//            'Брой заредени класове :'.$totalLoadClasses,'Не успях да заредя следните PHP-класове :'.$couldntLoadsClasses,
+//            'Брой методи :'.$totalMethods,'on_Methods :'.count($onMethods), array_values($onMethods));
 
-        return 'Брой линии : '.$totalLines."<br>".'Брой празни редове :'.$emptyLineCounter
-        .'<br>'.'Общо файлове :'.$filesCounter.'<br>'.' Брой заредени класове  : '.$totalLoadClasses
-        ."<br>".'Брой методи : '.$totalMethods;
+//bp(count($onMethods),arsort($onMethods),$onMethods);
+        return 'Общо файлове :'.$filesCounter.'<br>'.'Брой линии : '.$totalLines."<br>".'Брой празни редове :'.$emptyLineCounter
+        .'<br>'.' Брой заредени класове  : '.$totalLoadClasses."<br>".'Не успях да заредя следните PHP-класове :'.$couldntLoadsClasses
+        ."<br>".'Брой методи : '.$totalMethods."<br>".'on_Methods :'.count($onMethods).'<br>'.'<br>'.$onMethods_string;
+
     }
+
+
+
+
     /**
      * @param $root
      * @param array $files
      */
     static function readFiles($root, &$files = array())
+
     {
         if ($handle = opendir($root)) {
 
             while (FALSE !== ($entry = readdir($handle))) {
 
-                if ($entry == "." || $entry == "..") continue;
+                if ($entry == "." || $entry == ".." || $entry == "unit") continue;
 
                 $entry = $root . DIRECTORY_SEPARATOR . $entry;
 
@@ -138,7 +168,7 @@ class myself_Codebase extends core_Manager
      * @return bool
      */
 
-    static function loadClasses($root, $f, &$methods=array(),&$couldntLoadsClasses=array())
+    static function loadClasses($root, $f, &$methods=array(),&$couldntLoadsClasses=array(),&$onMethods=array())
     {
         //    return array('isLoaded' => TRUE, 'methods' => array(...));
 
@@ -154,16 +184,29 @@ class myself_Codebase extends core_Manager
 
             $className = str_replace(".class.php", '', $className);
 
-            if (@cls::load($className, TRUE)) {
+            if (@cls::load($className,TRUE)) {
                 ;
                 $classLoadResult = TRUE;
+
                 $class = new ReflectionClass($className);
+
                 $methods = $class->getMethods();
+
                 foreach ($methods as $id => $m) {
+
                     if (strtolower(trim($m->class)) != strtolower(trim($className))) {
+
                         unset($methods[$id]);
+
+                    }else{ if(substr($m->name,0,3) == 'on_'){
+
+                        $onMethods[substr($m->name,3)] = $m->name;
+                        //  $onMethods[$m->name]++;
+
+                    }
                     }
                 }
+
             } else {
                 $couldntLoadsClasses[] = $className;
             }
